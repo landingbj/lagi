8fd51b27ed5d0ab4991fccdfe1b75cf189951aba
linzhen <283541784@qq.com>
2024-04-30 16:59:33 +0800

    nav disable

--- lagi-web/src/main/java/ai/servlet/PageInfoServlet.java
+++ lagi-web/src/main/java/ai/servlet/PageInfoServlet.java
@@ -1,12 +1,12 @@
 package ai.servlet;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+import ai.common.pojo.Configuration;
 import ai.servlet.annotation.Get;
 import ai.servlet.annotation.Param;
 import ai.servlet.dto.Prompt;
+import ai.utils.MigrateGlobal;
 
 public class PageInfoServlet extends RestfulServlet{
 	
@@ -16,7 +16,9 @@ public class PageInfoServlet extends RestfulServlet{
 	private static final long serialVersionUID = 1L;
 	
 	public static List<Prompt> prompts = new ArrayList<>(10);
-	
+	private static final Configuration config = MigrateGlobal.config;
+	private static final Map<String, Integer> navMap = new HashMap<>(20);
+
 	static{
 		prompts.add(new Prompt("内容撰写","请为一位大学生写一篇产品助理岗位的实习报告"	));
 		prompts.add(new Prompt("内容改写","请帮我把这段内容改写：“天青色等烟雨，而我在等你”"));
@@ -26,11 +28,55 @@ public class PageInfoServlet extends RestfulServlet{
 		prompts.add(new Prompt("家常菜谱","请告诉我红烧猪蹄的做法和注意事项"));
 		prompts.add(new Prompt("社恐交际","应酬时想以茶代酒该怎么高情商发言？"));
 		prompts.add(new Prompt("“社恐”交际","如何不带脏字的骂人？"));
-		
+
+		// 智能问答 配置检测
+
+		long llmCount = config.getLLM().getBackends().stream().filter(backend -> {
+			return backend.getEnable();
+		}).count();
+		navMap.put("znwd", llmCount > 0 ? 1 :0);
+		navMap.put("wbsc", llmCount > 0 ? 1 :0);
+		long count = config.getASR().getBackends().stream().filter(backend -> {
+			return backend.getEnable();
+		}).count();
+		navMap.put("yysb", count > 0 ? 1 :0);
+		count = config.getTTS().getBackends().stream().filter(backend -> {
+			return backend.getEnable();
+		}).count();
+		navMap.put("qrqs", count > 0 ? 1 :0);
+		count = config.getImageCaptioning().getBackends().stream().filter(backend -> {
+			return backend.getEnable();
+		}).count();
+		navMap.put("ktsh", count > 0 ? 1 :0);
+		count = config.getImageEnhance().getBackends().stream().filter(backend -> {
+			return backend.getEnable();
+		}).count();
+		navMap.put("hzzq", count > 0 ? 1 :0);
+		count = config.getImageGeneration().getBackends().stream().filter(backend -> {
+			return backend.getEnable();
+		}).count();
+		navMap.put("tpsc", count > 0 ? 1 :0);
+		count = config.getVideoTrack().getBackends().stream().filter(backend -> {
+			return backend.getEnable();
+		}).count();
+		navMap.put("spzz", count > 0 ? 1 :0);
+		count = config.getVideoEnhance().getBackends().stream().filter(backend -> {
+			return backend.getEnable();
+		}).count();
+		navMap.put("spzq", count > 0 ? 1 :0);
+		count = config.getVideoGeneration().getBackends().stream().filter(backend -> {
+			return backend.getEnable();
+		}).count();
+		navMap.put("spsc", count > 0 ? 1 :0);
+		navMap.put("kjsx", config.getVectorStore() != null  ? 1 :0);
+		// aiservice
+		navMap.put("zlsc", 1);
+
+		navMap.put("twhp", llmCount > 0 ? 1 :0);
 	}
 	
 	public static List<Prompt> randomPrompts(int size) {
-		size = size > prompts.size() ? prompts.size() :size;
+		size = Math.min(size, prompts.size());
 		List<Prompt> res = new ArrayList<>(prompts);
 		Collections.shuffle(res);
 		return res.subList(0, size);
@@ -39,6 +85,11 @@ public class PageInfoServlet extends RestfulServlet{
 	@Get("getPrompts")
 	public List<Prompt> getPromptList(@Param("size") Integer size) {
 		return randomPrompts(size);
+	}
+
+	@Get("getNavStatus")
+	public Map<String, Integer> getNavStatus() {
+		return navMap;
 	}
 	
 }
