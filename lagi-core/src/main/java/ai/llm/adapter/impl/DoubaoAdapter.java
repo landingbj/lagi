27b8295d2b16c06661bc9fe0b788644d8c6ef409
tgs <guoshan.tong@landingbj.com>
2024-07-01 09:46:28 +0800

    update DobaoAdapter

--- lagi-core/src/main/java/ai/llm/adapter/impl/DoubaoAdapter.java
+++ lagi-core/src/main/java/ai/llm/adapter/impl/DoubaoAdapter.java
@@ -14,10 +14,19 @@ import com.volcengine.ark.runtime.service.ArkService;
 import io.reactivex.Flowable;
 import io.reactivex.Observable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-@LLM(modelNames = {"Doubao-pro-4k,Doubao-pro-32k"})
+@LLM(modelNames = {"doubao-pro-32k,doubao-pro-4k"})
 public class DoubaoAdapter  extends ModelService implements ILlmAdapter {
+
+
+    private String getModelEndpoint(String model) {
+        Map<String, String> collect = Arrays.stream(alias.split(",")).collect(Collectors.toMap(a -> a.split("=")[0], a -> a.split("=")[1]));
+        return collect.get(model);
+    }
     @Override
     public ChatCompletionResult completions(ChatCompletionRequest request) {
         ArkService service = ArkService.builder().apiKey(apiKey).baseUrl("https://ark.cn-beijing.volces.com/api/v3/").build();
@@ -28,7 +37,7 @@ public class DoubaoAdapter  extends ModelService implements ILlmAdapter {
         messages.add(userMessage);
 
         com.volcengine.ark.runtime.model.completion.chat.ChatCompletionRequest chatCompletionRequest = com.volcengine.ark.runtime.model.completion.chat.ChatCompletionRequest.builder()
-                .model(getModel())
+                .model(getModelEndpoint(model))
                 .messages(messages)
                 .build();
         com.volcengine.ark.runtime.model.completion.chat.ChatCompletionResult chatCompletion = service.createChatCompletion(chatCompletionRequest);
@@ -48,9 +57,12 @@ public class DoubaoAdapter  extends ModelService implements ILlmAdapter {
         messages.add(userMessage);
 
         com.volcengine.ark.runtime.model.completion.chat.ChatCompletionRequest streamChatCompletionRequest = com.volcengine.ark.runtime.model.completion.chat.ChatCompletionRequest.builder()
-                .model(getModel())
+                .model(getModelEndpoint(model))
                 .messages(messages)
                 .build();
+
+        System.out.println("model:"+model);
+        System.out.println("model1:"+getModelEndpoint(model));
 
         Flowable<ChatCompletionChunk> flowable = service.streamChatCompletion(streamChatCompletionRequest);
         Observable<ChatCompletionResult> iterable = Observable.create(observableEmitter -> {
