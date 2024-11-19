7208a4ec8b144e43b690e9dd4bc7bc593d28bf5b
yinruoxi666 <403155798@qq.com>
2024-11-19 21:19:27 +0800

    feature:   1.提示词工厂代码开发。

--- lagi-core/src/main/java/ai/config/pojo/PromptConfig.java
+++ lagi-core/src/main/java/ai/config/pojo/PromptConfig.java
@@ -4,10 +4,22 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+import java.util.List;
+
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 @Data
 public class PromptConfig {
     @JsonProperty("prompt")
-    private Boolean enable;
+    private Prompt prompt;
+    @Data
+    public static class Prompt {
+        private Boolean enable;
+        private List<Role> roles;
+    }
+    @Data
+    public static class Role {
+        private String name;
+        private String prompt;
+    }
 
 }
--- lagi-core/src/main/java/ai/prompt/PromptFactory.java
+++ lagi-core/src/main/java/ai/prompt/PromptFactory.java
@@ -1,4 +1,64 @@
 package ai.prompt;
 
+import ai.config.pojo.PromptConfig;
+import ai.openai.pojo.ChatCompletionRequest;
+import ai.openai.pojo.ChatMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class PromptFactory {
+    String prompt = "input your prompt here %s";
+    private PromptConfig promptConfig;
+    private static final Logger log = LoggerFactory.getLogger(PromptFactory.class);
+
+    public ChatCompletionRequest loadPrompt(ChatCompletionRequest request) {
+        loadContext();
+        ChatMessage message = request.getMessages().get(request.getMessages().size() - 1);
+        String prompt = promptConfig.getPrompt().getRoles().stream()
+                .findFirst()
+                .map(PromptConfig.Role::getPrompt)
+                .orElse("%s");
+        message.setContent(String.format(prompt, message.getContent()));
+        return request;
+    }
+
+    public ChatCompletionRequest loadPrompt(ChatCompletionRequest request,String roleName) {
+        loadContext();
+        ChatMessage message = request.getMessages().get(request.getMessages().size() - 1);
+        String prompt = promptConfig.getPrompt().getRoles().stream()
+                .filter(role -> role.getName().equals(roleName))
+                .findFirst()
+                .map(PromptConfig.Role::getPrompt)
+                .orElse("%s");
+        message.setContent(String.format(prompt, message.getContent()));
+        return request;
+    }
+    private void loadContext() {
+        try {
+            InputStream resourceAsStream = Files.newInputStream(Paths.get("lagi-web/src/main/resources/prompt_template.yml"));
+            ObjectMapper mapper = new YAMLMapper();
+            mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+            promptConfig = mapper.readValue(resourceAsStream, PromptConfig.class);
+
+        } catch (IOException e) {
+            log.warn(e.getMessage());
+        }
+    }
+    public PromptConfig getPromptConfig() {
+        return promptConfig;
+    }
+
+    public static void main(String[] args) {
+        PromptFactory promptFactory = new PromptFactory();
+        promptFactory.loadContext();
+        log.info("{}",promptFactory.promptConfig);
+    }
 }
