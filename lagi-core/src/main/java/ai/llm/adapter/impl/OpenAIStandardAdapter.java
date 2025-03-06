9e7e1eb3d773a4a486f7b1258051697ed4145dda
tgs <guoshan.tong@landingbj.com>
2025-03-06 15:54:00 +0800

    update Adapter

--- lagi-core/src/main/java/ai/llm/adapter/impl/OpenAIStandardAdapter.java
+++ lagi-core/src/main/java/ai/llm/adapter/impl/OpenAIStandardAdapter.java
@@ -9,9 +9,16 @@ import ai.llm.utils.OpenAiApiUtil;
 import ai.llm.utils.convert.GptConvert;
 import ai.openai.pojo.ChatCompletionRequest;
 import ai.openai.pojo.ChatCompletionResult;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import io.reactivex.Observable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @LLM(modelNames = {"*"})
@@ -46,8 +53,35 @@ public class OpenAIStandardAdapter extends ModelService implements ILlmAdapter {
     }
 
     private void setDefaultModel(ChatCompletionRequest request) {
-        if (request.getModel() == null) {
-            request.setModel(getModel());
+        try {
+            String url = "http://127.0.0.1:8800/v1/models";
+            String response = HttpUtil.get(url);
+            List<String> llmIds = extractLlmIds(response);
+                request.setModel(llmIds.get(0));
+        }catch (Exception e){
+            if (request.getModel() == null) {
+                request.setModel(getModel());
+                System.out.println("8800未找到对应的模型 llmIds:");
+            }
         }
+
+    }
+    private static List<String> extractLlmIds(String jsonResponse) {
+        List<String> llmIds = new ArrayList<>();
+        if (JSONUtil.isJson(jsonResponse)) {
+            JSONObject jsonObject = JSONUtil.parseObj(jsonResponse);
+            JSONArray dataArray = jsonObject.getJSONArray("data");
+            if (dataArray != null) {
+                for (Object obj : dataArray) {
+                    JSONObject dataObject = (JSONObject) obj;
+                    String modelType = dataObject.getStr("model_type");
+                    if ("LLM".equals(modelType)) {
+                        String id = dataObject.getStr("id");
+                        llmIds.add(id);
+                    }
+                }
+            }
+        }
+        return llmIds;
     }
 }
--- lagi-core/src/main/java/ai/llm/adapter/impl/VicunaAdapter.java
+++ lagi-core/src/main/java/ai/llm/adapter/impl/VicunaAdapter.java
@@ -5,7 +5,7 @@ import ai.annotation.LLM;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//@LLM(modelNames = {"vicuna-13b","vicuna-7b","vicuna-7b-16k","vicuna-13B-16k","vicuna-33B","电通大模型"})
+//@LLM(modelNames = {"vicuna-13b","vicuna-7b","vicuna-7b-16k","vicuna-13B-16k","vicuna-33B"})
 @LLM(modelNames = {"DeepSeek-R1","智谱Edge-1.5B","拉马3.2-1B","智谱4-9B","DeepSeek-R1-8B","DeepSeek-R1-7B"})
 public class VicunaAdapter extends OpenAIStandardAdapter {
     private static final Logger logger = LoggerFactory.getLogger(VicunaAdapter.class);
