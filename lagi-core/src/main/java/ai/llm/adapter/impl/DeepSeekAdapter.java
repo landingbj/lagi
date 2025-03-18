ece32b9f9502f9d8783d505757d7855f97bf231d
Shawn <xiuwen.zhao@landingbj.com>
2025-03-18 11:41:02 +0800

    Update DeepSeekAdapter.java

--- lagi-core/src/main/java/ai/llm/adapter/impl/DeepSeekAdapter.java
+++ lagi-core/src/main/java/ai/llm/adapter/impl/DeepSeekAdapter.java
@@ -1,15 +1,16 @@
 package ai.llm.adapter.impl;
 
-import ai.annotation.LLM;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@LLM(modelNames = {"deepseek-chat"})
 public class DeepSeekAdapter extends OpenAIStandardAdapter {
     private static final Logger logger = LoggerFactory.getLogger(DeepSeekAdapter.class);
 
     @Override
     public String getApiAddress() {
-        return "https://api.deepseek.com/chat/completions";
+        if (apiAddress == null) {
+            apiAddress = "https://api.deepseek.com/chat/completions";
+        }
+        return apiAddress;
     }
 }
