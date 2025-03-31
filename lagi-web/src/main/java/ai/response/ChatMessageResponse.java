4008d61a39f16ec1943311cef232928533cc53b5
tgs <guoshan.tong@landingbj.com>
2025-03-31 15:38:13 +0800

    update isRag

--- lagi-web/src/main/java/ai/response/ChatMessageResponse.java
+++ lagi-web/src/main/java/ai/response/ChatMessageResponse.java
@@ -10,7 +10,6 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Builder
 public class ChatMessageResponse extends ChatMessage {
     private List<String> contextChunkIds;
 }
