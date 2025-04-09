21d1ed2603c50cf16816940589ac0b332af22174
linzhen <283541784@qq.com>
2025-04-09 14:39:11 +0800

    modify vector intent detect

--- lagi-core/src/main/java/ai/intent/impl/VectorIntentServiceImpl.java
+++ lagi-core/src/main/java/ai/intent/impl/VectorIntentServiceImpl.java
@@ -6,6 +6,7 @@ import ai.intent.enums.IntentStatusEnum;
 import ai.intent.pojo.IntentResult;
 import ai.manager.EmbeddingManager;
 import ai.openai.pojo.ChatCompletionRequest;
+import ai.openai.pojo.ChatMessage;
 import ai.utils.CosineSimilarityUtil;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,17 +21,25 @@ public class VectorIntentServiceImpl extends SampleIntentServiceImpl {
     @Override
     protected void setIntentByVector(ChatCompletionRequest chatCompletionRequest, Integer lIndex, String lastQ, IntentResult intentResult) {
         Embeddings embeddings = EmbeddingManager.getInstance().getAdapter();
-        String lQ = chatCompletionRequest.getMessages().get(lIndex).getContent();
-        List<Float> embedding1 = embeddings.createEmbedding(lQ);
         List<Float> embedding2 = embeddings.createEmbedding(lastQ);
-        double v1 = CosineSimilarityUtil.calculateCosineSimilarity(embedding1, embedding2);
-        log.info("lQ: {}\n lastQ: {} \n v1:{}", lQ, lastQ, v1);
-        if(v1 > 0.6) {
-            intentResult.setStatus(IntentStatusEnum.CONTINUE.getName());
-            intentResult.setContinuedIndex(lIndex);
-        } else {
-            intentResult.setStatus(IntentStatusEnum.COMPLETION.getName());
+        for (int i = 0; i < chatCompletionRequest.getMessages().size() - 1; i++) {
+            if(i < lIndex) {
+                continue;
+            }
+            ChatMessage chatMessage = chatCompletionRequest.getMessages().get(i);
+            if(!chatMessage.getRole().equals("user")) {
+                continue;
+            }
+            List<Float> embedding1 = embeddings.createEmbedding(chatMessage.getContent());
+            double v1 = CosineSimilarityUtil.calculateCosineSimilarity(embedding1, embedding2);
+            log.info("lQ: {}\n lastQ: {} \n v1:{}", chatMessage.getContent(), lastQ, v1);
+            if(v1 > 0.55) {
+                intentResult.setStatus(IntentStatusEnum.CONTINUE.getName());
+                intentResult.setContinuedIndex(lIndex);
+                return;
+            }
         }
+        intentResult.setStatus(IntentStatusEnum.COMPLETION.getName());
     }
 
 }
