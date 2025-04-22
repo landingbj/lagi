b4d28191dac144679c7e5e786f308b465c084f84
Shawn <xiuwen.zhao@landingbj.com>
2025-04-22 16:24:20 +0800

    Update FlytekAdapter.java

--- lagi-core/src/main/java/ai/wrapper/impl/FlytekAdapter.java
+++ lagi-core/src/main/java/ai/wrapper/impl/FlytekAdapter.java
@@ -11,11 +11,12 @@ public class FlytekAdapter extends CommonAdapter implements IWrapper {
     @Override
     protected Map<String, String> getModelDriverMap() {
         Map<String, String> modelDriverMap = new HashMap<>();
-        modelDriverMap.put("v1.1", "ai.llm.adapter.impl.SparkAdapter");
-        modelDriverMap.put("v2.1", "ai.llm.adapter.impl.SparkAdapter");
-        modelDriverMap.put("v3.1", "ai.llm.adapter.impl.SparkAdapter");
-        modelDriverMap.put("v3.5", "ai.llm.adapter.impl.SparkAdapter");
-        modelDriverMap.put("v4.0", "ai.llm.adapter.impl.SparkAdapter");
+        modelDriverMap.put("lite", "ai.llm.adapter.impl.SparkAdapter");
+        modelDriverMap.put("generalv3", "ai.llm.adapter.impl.SparkAdapter");
+        modelDriverMap.put("pro-128k", "ai.llm.adapter.impl.SparkAdapter");
+        modelDriverMap.put("generalv3.5", "ai.llm.adapter.impl.SparkAdapter");
+        modelDriverMap.put("max-32k", "ai.llm.adapter.impl.SparkAdapter");
+        modelDriverMap.put("4.0Ultra", "ai.llm.adapter.impl.SparkAdapter");
 
         modelDriverMap.put("tti", "ai.image.adapter.impl.SparkImageAdapter");
         return modelDriverMap;
