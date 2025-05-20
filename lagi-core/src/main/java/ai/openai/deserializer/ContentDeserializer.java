657b1ed1396b2fd04b47d5361da82c552c9b0eb5
Shawn <xiuwen.zhao@landingbj.com>
2025-05-20 15:47:33 +0800

    Update ContentDeserializer.java

--- lagi-core/src/main/java/ai/openai/deserializer/ContentDeserializer.java
+++ lagi-core/src/main/java/ai/openai/deserializer/ContentDeserializer.java
@@ -11,6 +11,6 @@ public class ContentDeserializer extends JsonDeserializer<String> {
     @Override
     public String deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
         JsonNode node = jp.getCodec().readTree(jp);
-        return node.toString();
+        return node.asText();
     }
 }
